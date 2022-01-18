63667a754f75095e4a62b6b7eed864a4683b216b
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-01-18 22:55:56 +0000

    create spinner

---
+++ spinner1.js
@@ -0,0 +1,39 @@
+setTimeout(() => {
+  process.stdout.write("\r|   ");
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write("\r/   ");
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write("\r-   ");
+}, 500);
+
+setTimeout(() => {
+  process.stdout.write("\r\\   ");
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write("\r|   ");
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write("\r/   ");
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write("\r-   ");
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write("\r\\   ");
+}, 1700);
+
+setTimeout(() => {
+  process.stdout.write("\r|   ");
+}, 2000);
+
+setTimeout(() => {
+  console.log();
+}, 2300);
